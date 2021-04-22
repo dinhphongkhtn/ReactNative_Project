e02971047ac2038512086e32f833d273ef41c07d
dinhphongkhtn <dinhphongkhtn@gmail.com>
2021-04-23 06:22:33 +0700

    button

--- LessionProjects/App.js
+++ LessionProjects/App.js
@@ -1,11 +1,26 @@
-import React from 'react';
-import { View, StyleSheet } from "react-native";
+import React, { Component } from 'react';
+import { View, StyleSheet, Button, TouchableHighlight, Text } from "react-native";
+
+
+const _onPressButton = () => {
+  alert('You tapped the button!')
+};
 const App = () => {
 
   return (
     <>
       <View style={[styles.container, styles.verticalContainer]}>
         <View style={[styles.box, styles.yellowBox]}>
+          <View style={styles.button}>
+            <TouchableHighlight style={{ justifyContent: 'center',flex:1, backgroundColor:'purple'}}
+              onPress={_onPressButton} underlayColor="red">
+              <Text style={styles.buttonText}>Touch Me!</Text>
+            </TouchableHighlight>
+            <TouchableHighlight style={{ justifyContent: 'center',flex:1, backgroundColor:'gray' }}
+              onPress={_onPressButton} underlayColor="red">
+              <Text style={styles.buttonText}>Touch Me!</Text>
+            </TouchableHighlight>
+          </View>
         </View>
         <View style={[styles.box, styles.yellowBox]}>
         </View>
@@ -14,6 +29,13 @@ const App = () => {
       <View style={[styles.container, styles.horizontalContainer]}>
         <View style={styles.container}>
           <View style={[styles.box, styles.blueBox]}>
+            <View style={styles.button}>
+              <TouchableHighlight style={{  justifyContent: 'center',flex:1 }}
+                onPress={_onPressButton} underlayColor="red">
+                <Text style={styles.buttonText}>Touch Me!</Text>
+              </TouchableHighlight>
+              
+            </View>
           </View>
           <View style={[styles.box, styles.blueBox]}>
           </View>
@@ -29,17 +51,20 @@ const App = () => {
   );
 };
 
+
+
 const styles = StyleSheet.create({
-  container: {    
+  container: {
     flex: 1,
-    
+
   },
 
   box: {
 
     borderRadius: 20,
     flex: 1,
-    margin:20
+    margin: 20,
+    padding: 10
   },
   yellowBox: {
     backgroundColor: '#ffffc2',
@@ -47,23 +72,36 @@ const styles = StyleSheet.create({
   },
   blueBox: {
     backgroundColor: '#4f82c0',
-    
+
   },
-  verticalContainer:{
+  verticalContainer: {
     flexDirection: 'column',
-    backgroundColor: '#A1c99A', 
+    backgroundColor: '#A1c99A',
     marginBottom: 10,
-    borderBottomLeftRadius:20,
-    borderBottomRightRadius:20,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
   },
-  horizontalContainer:{
-    flexDirection:'row',
-    backgroundColor: '#c8c8fA', 
-    marginTop: 10 ,
-    borderTopLeftRadius:20,
-    borderTopRightRadius:20,
+  horizontalContainer: {
+    flexDirection: 'row',
+    backgroundColor: '#c8c8fA',
+    marginTop: 10,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
   },
-  
+  button: {
+
+    backgroundColor: '#2196F3',
+    borderRadius: 15,
+    flex: 1,
+    flexDirection: 'row',
+    marginHorizontal:10,
+    justifyContent:'space-around',
+    padding:10
+  },
+  buttonText: {
+    textAlign: 'center',
+    color: 'white'
+  }
 });
 
 export default App;
