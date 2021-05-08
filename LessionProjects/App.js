642cd4b8b174e4b3cd03714892c18c606f930864
dinhphongkhtn <dinhphongkhtn@gmail.com>
2021-05-08 11:05:22 +0700

    Baitap

--- LessionProjects/App.js
+++ LessionProjects/App.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react'
 import { Text, View, StyleSheet } from 'react-native'
 import HeroList from './src/components/HeroList'
-
+import Entypo from "./src/assets/icons/index";
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <View style={[styles.titleContainer,{flex:1.2}]}>
-          <Text style={styles.titleText}>Tìm kiếm gần đây</Text>
+        <View style={[styles.titleContainer, { flex: 1.2 }]}>
+        <Text style={styles.titleText}>Tìm kiếm gần đây</Text>
           <HeroList displayMode={1} horizontal={true}></HeroList>
         </View>
 
-        <View style={[styles.titleContainer,{flex:5}]}>
+        <View style={[styles.titleContainer, { flex: 5 }]}>
           <Text style={styles.titleText}>Gợi ý bạn bè</Text>
           <HeroList displayMode={2} horizontal={false}></HeroList>
         </View>
+
+        {/* <Entypo name="shoppingcart" size={30} color={'red'}></Entypo> */}
       </View>
     )
   }
@@ -26,14 +28,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#f2f2f2'
   },
   titleContainer: {
-    
-    flex:1,
+
+    flex: 1,
     justifyContent: 'space-between'
   },
   titleText: {
     fontWeight: 'bold',
     fontSize: 14,
-    marginVertical:10
+    marginVertical: 10
   }
 
 })
---
+++ LessionProjects/src/assets/icons/index.js
@@ -0,0 +1,3 @@
+import Entypo from "react-native-vector-icons/AntDesign";
+
+export default Entypo;
